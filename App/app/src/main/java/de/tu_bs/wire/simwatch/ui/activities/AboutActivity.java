26e687bb2be56de69f650ecb5fc1636064950c7c
Tran Van Tuan <tran.van.tuan@gmx.de>
2016-07-06 15:26:59 +0200

    [app] Added information to app settings

---
+++ App/app/src/main/java/de/tu_bs/wire/simwatch/ui/activities/AboutActivity.java
@@ -0,0 +1,15 @@
+package de.tu_bs.wire.simwatch.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import de.tu_bs.wire.simwatch.R;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
